3868f65fe4e78cb900f35abf34ab52b3b4fc24bb
Jouni Alanen <jouni@diory.me>
2024-10-18 22:51:20 +0300

    /test-s3 endpoint to verify S3 credentials

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { ListObjectsV2Command, S3Client } from '@aws-sdk/client-s3';
+import { Controller, Get, Session } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
   index() {
     return 'This is index!!!!!!!!!!"';
+  }
+
+  @Get('test-s3')
+  async testAction(@Session() session: Record<string, string>) {
+    const options = {
+      region: process.env.AWS_REGION,
+      credentials: JSON.parse(session.awsCredentials),
+    };
+
+    const s3Client = new S3Client(options);
+
+    const listCommand = new ListObjectsV2Command({
+      Bucket: 'diory-mobile-proto',
+      Prefix: '',
+    });
+
+    const list = await s3Client.send(listCommand);
+
+    return JSON.stringify(list);
   }
 }
--- src/throwaway/throwaway.controller.ts
+++ src/throwaway/throwaway.controller.ts
@@ -7,25 +7,6 @@ import { Request } from 'express';
 export class ThrowawayController {
   constructor(@Inject('REDIS_CLIENT') private readonly redisClient: Redis) {}
 
-  @Get('test')
-  async testAction(@Session() session: Record<string, string>) {
-    const options = {
-      region: 'eu-west-1',
-      credentials: JSON.parse(session.awsCredentials),
-    };
-
-    const s3Client = new S3Client(options);
-
-    const listCommand = new ListObjectsV2Command({
-      Bucket: 'jvalanen-diory-test3',
-      Prefix: 'room/',
-    });
-
-    const list = await s3Client.send(listCommand);
-
-    return JSON.stringify(list);
-  }
-
   @Get('rooms')
   async roomsAction(@Session() session: Record<string, string>) {
     const response = await this.redisClient.keys(`${session.userId}-rooms-*`);
