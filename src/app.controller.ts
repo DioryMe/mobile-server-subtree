938565bd6dd44bbe63de70bbc4f59a25bf08f500
Jouni Alanen <jouni@diory.me>
2024-10-19 15:15:35 +0300

    User gets and lists own home folder in S 3
    - /test-list-s3 and /test-get-s3
    - works with more broad policy (TODO to restrict it for user's own folder only...)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,8 @@
-import { ListObjectsV2Command, S3Client } from '@aws-sdk/client-s3';
+import {
+  GetObjectCommand,
+  ListObjectsV2Command,
+  S3Client,
+} from '@aws-sdk/client-s3';
 import { Controller, Get, Session } from '@nestjs/common';
 
 @Controller()
@@ -13,8 +17,8 @@ export class AppController {
     return session.userId;
   }
 
-  @Get('test-s3')
-  async testAction(@Session() session: Record<string, string>) {
+  @Get('test-list-s3')
+  async testListAction(@Session() session: Record<string, string>) {
     const options = {
       region: process.env.AWS_REGION,
       credentials: JSON.parse(session.awsCredentials),
@@ -24,11 +28,42 @@ export class AppController {
 
     const listCommand = new ListObjectsV2Command({
       Bucket: 'diory-mobile-proto',
-      Prefix: session.userId,
+      Prefix: `${session.userId}`,
     });
 
     const list = await s3Client.send(listCommand);
 
     return JSON.stringify(list);
   }
+
+  @Get('test-get-s3')
+  async testGetAction(@Session() session: Record<string, string>) {
+    const options = {
+      region: process.env.AWS_REGION,
+      credentials: JSON.parse(session.awsCredentials),
+    };
+
+    const s3Client = new S3Client(options);
+
+    const command = new GetObjectCommand({
+      Bucket: 'diory-mobile-proto',
+      Key: `${session.userId}/package.json`,
+    });
+
+    const response = await s3Client.send(command);
+
+    const bodyContents = await streamToString(response.Body);
+
+    return bodyContents;
+  }
+}
+
+// Helper function to convert a readable stream to a string
+async function streamToString(stream: any) {
+  return new Promise((resolve, reject) => {
+    const chunks: any = [];
+    stream.on('data', (chunk: any) => chunks.push(chunk));
+    stream.on('error', reject);
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf-8')));
+  });
 }
